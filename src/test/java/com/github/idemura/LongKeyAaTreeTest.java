73ac6ce1e7551e7a1f9b2914e1ececcf71a590da
Igor Demura <igor.demura@gmail.com>
2026-03-13 12:21:57 -0700

    Small test

--- src/test/java/com/github/idemura/LongKeyAaTreeTest.java
+++ src/test/java/com/github/idemura/LongKeyAaTreeTest.java
@@ -5,9 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class LongKeyAaTreeTest {
+  private static void putAndVerify(LongKeyAaTree<String> t, long key) {
+    t.put(key, Long.toString(key));
+    t.verify();
+  }
+
   @Test
-  void testPut() {
+  void testPrimitiveShapes() {
     var t = new LongKeyAaTree<String>();
+    t.verify();
     assertEquals(0, t.size());
+    putAndVerify(t, 50);
+    assertEquals(1, t.size());
+    assertEquals("50", t.get(50));
+    // Skew
+    putAndVerify(t, 40);
+    assertEquals("40", t.get(40));
+    assertEquals("50", t.get(50));
+    // Skew+split
+    putAndVerify(t, 30);
+    assertEquals("30", t.get(30));
+    assertEquals("40", t.get(40));
+    assertEquals("50", t.get(50));
   }
 }
