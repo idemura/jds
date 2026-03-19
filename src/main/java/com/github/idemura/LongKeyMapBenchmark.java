9ce3753e6198be02a026bf40756b3afc1974522d
Igor Demura <igor.demura@gmail.com>
2026-03-18 23:43:25 -0700

    Use List

--- src/main/java/com/github/idemura/LongKeyMapBenchmark.java
+++ src/main/java/com/github/idemura/LongKeyMapBenchmark.java
@@ -2,9 +2,11 @@ package com.github.idemura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
+import java.util.random.RandomGenerator;
 import org.openjdk.jmh.annotations.*;
 
 @BenchmarkMode(Mode.AverageTime)
@@ -14,33 +16,41 @@ import org.openjdk.jmh.annotations.*;
 @Fork(3)
 public class LongKeyMapBenchmark {
   private static final int SIZE = 100_000;
-  private static final int SEED = (int) System.nanoTime();
 
   static LongKeyMap<String> makeCustomTreeMap() {
-    return new LongKeyAaTree<>();
+    // return new LongKeyAaTree<>();
     // return new LongKeySkipList<>();
+    return new LongKeyTreap<>(new Random());
+  }
+
+  private static List<Long> makeKeys(RandomGenerator random, int size) {
+    var keys = new ArrayList<Long>(size);
+    for (int i = 0; i < size; i++) {
+      keys.add(random.nextLong());
+    }
+    return keys;
   }
 
   @State(Scope.Thread)
   public static class InsertState {
-    public ArrayList<Long> keys;
+    public List<Long> keys;
 
     @Setup(Level.Trial)
     public void setup() {
-      var random = new Random(SEED);
+      var random = new Random(0);
       keys = makeKeys(random, SIZE);
     }
   }
 
   @State(Scope.Thread)
   public static class RemoveState {
-    public ArrayList<Long> keys;
+    public List<Long> keys;
     public LongKeyMap<String> customTreeMap;
     public TreeMap<Long, String> treeMap;
 
     @Setup(Level.Invocation)
     public void setup() {
-      var random = new Random(SEED);
+      var random = new Random(0);
       keys = makeKeys(random, SIZE);
 
       customTreeMap = makeCustomTreeMap();
@@ -50,7 +60,7 @@ public class LongKeyMapBenchmark {
         treeMap.put(key, "dummy");
       }
 
-      // Shuffle randomly for removes.
+      // Shuffle keys for remove order.
       Collections.shuffle(keys, random);
     }
   }
@@ -73,27 +83,19 @@ public class LongKeyMapBenchmark {
     return tree;
   }
 
-  @Benchmark
-  public Object customTreeMapRemove(RemoveState state) {
-    for (long key : state.keys) {
-      state.customTreeMap.remove(key);
-    }
-    return state.customTreeMap;
-  }
-
-  @Benchmark
-  public Object treeMapRemove(RemoveState state) {
-    for (long key : state.keys) {
-      state.treeMap.remove(key);
-    }
-    return state.treeMap;
-  }
-
-  private static ArrayList<Long> makeKeys(Random random, int size) {
-    var keys = new ArrayList<Long>(size);
-    for (int i = 0; i < size; i++) {
-      keys.add(random.nextLong());
-    }
-    return keys;
-  }
+  // @Benchmark
+  // public Object customTreeMapRemove(RemoveState state) {
+  //   for (long key : state.keys) {
+  //     state.customTreeMap.remove(key);
+  //   }
+  //   return state.customTreeMap;
+  // }
+  //
+  // @Benchmark
+  // public Object treeMapRemove(RemoveState state) {
+  //   for (long key : state.keys) {
+  //     state.treeMap.remove(key);
+  //   }
+  //   return state.treeMap;
+  // }
 }
